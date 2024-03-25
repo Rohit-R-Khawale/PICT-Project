1df5e2667c35808d48bb76ae6f639e6031379175
Rohit-R-Khawale <rohitrkhawale@gmail.com>
2024-03-25 14:08:15 +0530

    Entire Homepage ready without review section/ commit 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import Navbar from "./Components/Navbar"
+import "./App.css"
 function App() {
-  
+
   return (
     <div>
-      <Navbar/>
+      <div className="homePageUL">
+        <div className="NavContainer"><Navbar/></div>
+        {/* Tagline */}
+        <div className="tagLineHeading">
+          <h1 className="tagLine">Navigating Path To Better Health</h1>
+            <div className="loginButton">
+              <button value="login" id="loginBtn">Login to Continue</button>
+            </div>
+        </div>
+        
+        {/* Review Section */}
+        <div class="reviewSection">
+              <div class="back">
+              </div>
+              <div class="reviewContent">
+                  <div class="ratings">
+                      <p>Atharva Joshi: Rating with img</p>
+                      <p>Rohit Khawale: Rating with img</p>
+                      <p>Vaishnavi Mankar: Rating with img</p>
+                      <p>Shubham: Rating with img</p>
+                  </div>
+              </div>
+            </div>
+      </div>
+    
     </div>
   )
 }
